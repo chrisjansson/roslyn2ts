8ad0e0411b795cf2a1c6234b3fef1ee677ff53ce
Chris Jansson <chris.janssom@gmail.com>
2017-12-29 13:51:43 +0100

    Removed razorlight dep p2

--- CodeGen/RazorLightTemplateEngine.cs
+++ CodeGen/RazorLightTemplateEngine.cs
@@ -1,23 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using CodeGen.Abstract;
 using CodeGen.SimplifiedAst;
-using RazorLight;
 
 namespace CodeGen
 {
-    public class RazorLightTemplateEngineFactory
-    {
-        public RazorLightEngine Create(string directory)
-        {
-            return new RazorLightEngineBuilder()
-                .UseFilesystemProject(directory)
-                .UseMemoryCachingProvider()
-                .Build();
-        }
-    }
-
     public class TypescriptModel
     {
         public AbstractModule CurrentModule { get; set; }
